ca3a0763109e9faa37b626f97303e952e3a0978b
AlejandroMC-byte <alejandro.montero@correounivalle.edu.co>
2023-02-22 12:09:47 -0500

    pirmera seccion de login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import '../styles/global.scss';
+import Layout from '../containers/Layout'
+import Login from '../containers/login'
+import '../styles/global.css';
 
 const App = () => {
   return (
-    <h1>Hola mundo, Bienvenidos al curso de react</h1>
+    <Layout>
+      <Login  />
+    </Layout>
   )
 }
 
